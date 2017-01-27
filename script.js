4d7e10f27cfe3dc4183647bc5fbb4e0db18b7446
jinselambrechts <jinselambrechts@users.noreply.github.com>
2017-01-27 17:08:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -68,23 +68,6 @@ var iso = new Isotope( '.grid', {
     });
   }
 
-  // bind sort button click
-  var sortByGroup = document.querySelector('.sort-by-button-group');
-  sortByGroup.addEventListener( 'click', function( event ) {
-    // only button clicks
-    if ( !matchesSelector( event.target, '.button' ) ) {
-      return;
-    }
-    var sortValue = event.target.getAttribute('data-sort-value');
-    iso.arrange({ sortBy: sortValue });
-  });
-
-  // change is-checked class on buttons
-  var buttonGroups = document.querySelectorAll('.button-group');
-  for ( var i=0; i < buttonGroups.length; i++ ) {
-    buttonGroups[i].addEventListener( 'click', onButtonGroupClick );
-  }
-
   function onButtonGroupClick( event ) {
     // only button clicks
     if ( !matchesSelector( event.target, '.button' ) ) {
